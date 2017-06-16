85342d5d135be74f1f68545a3527f2c7233fee8b
Anne Marie King <amking23@gmail.com>
2017-06-16 11:16:27 -0500

    Set up js, html

---
+++ app.js
@@ -0,0 +1,25 @@
+var nunjucks = require("nunjucks");
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var express = require('express');
+var router = require('index.js');
+
+var app = express();
+const logger = morgan('dev');
+app.use(logger);
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+app.use(jsonParser);
+app.use(urlencodedParser);
+app.engine('html', nunjucks.render);
+app.set('view engine', 'html');
+nunjucks.configure({
+	autoescape: true,
+	express: app,
+	watch: true,
+	noCache: true
+});
+
+app.use('/', router);
+app.use(express.static('public'));
---
+++ index.js
@@ -0,0 +1,6 @@
+var express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, resp){
+	resp.render('index');
+});
