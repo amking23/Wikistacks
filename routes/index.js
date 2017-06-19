8d7c68bcc3427023043a0b23fffb333c11bc2693
Anne Marie King <amking23@gmail.com>
2017-06-19 09:54:01 -0500

    Starting Authors

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,13 @@ var express = require('express');
 const userRouter = require('./user.js')
 const wikiRouter = require('./wiki.js')
 const router = express.Router();
+var models = require('../models')
+var Page = models.Page
+var User = models.User
 
 router.get('/', function(req, resp){
-	resp.render('index');
+	Page.findAll()
+	.then(pages => resp.render('index', {pages: pages}))
 });
 
 router.use('/user',userRouter)
